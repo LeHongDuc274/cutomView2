d125a9d57bdfaad81b67beb90b55ed9dc3f54538
Le Duc <55406484+LeHongDuc274@users.noreply.github.com>
2021-12-29 18:10:31 +0700

    done

--- app/src/main/java/com/example/balls/customview/Ball.kt
+++ app/src/main/java/com/example/balls/customview/Ball.kt
@@ -15,91 +15,46 @@ class Ball(
     val pos: Int = 0,
     val numBalls: Int = 5,
     val colorBall: Int = Color.GREEN,
-    context: Context
-) :
-    View(context) {
-    private var mX: Int = 0
-    var mY: Int = 0
+    val centerX : Int,
+    val centerY : Int
+)
+{
+    var mX: Int
+    var mY: Int
+    var direction = 1
+    init {
+        mX = centerX
+        mY = centerY
+    }
+
 
     private val speed = DEFAULT_SPEED
 
-    var callBack: ((Int) -> Unit)? = null
-    val mPaintBall = Paint().apply {
-        setColor(colorBall)
-        style = Paint.Style.FILL_AND_STROKE
-        strokeWidth = 10F
-        strokeCap = Paint.Cap.ROUND
-        isAntiAlias = true
-    }
+    var callBack: ((Int,Int) -> Unit)? = null
     private var radius: Float = 0F
-    private var initCoordinatesX = 0
-    private var initCoordinatesY = 0
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val mWidth = MeasureSpec.getSize(widthMeasureSpec) / numBalls
-        val mHeight = MeasureSpec.getSize(heightMeasureSpec)
-        initCoordinatesY = mHeight / 2
-        initCoordinatesX = mWidth / 2
-        radius = mWidth / 5F
-        mX = initCoordinatesX
-        mY = initCoordinatesY
-        setMeasuredDimension(mWidth, mHeight)
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        drawBall(canvas)
-    }
-
-    private fun drawBall(canvas: Canvas?) {
-        canvas?.drawCircle(mX.toFloat(), mY.toFloat(), radius, mPaintBall)
-        invalidate()
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        if (pos == 0) {
-            when (event?.action) {
-                ACTION_DOWN -> {
-                    Log.e("size", event.y.toString())
-                    job?.cancel()
-                }
-
-                ACTION_MOVE -> {
-                    mY = event.y.toInt()
-                    callBack?.invoke(mY)
-                    invalidate()
-                }
-
-                ACTION_UP -> {
-                    reset(event.y.toInt())
-                }
-            }
-            return true
-        } else {
-            return true
-        }
-    }
 
     private var job: Job? = null
-    private fun reset(currenY: Int) {
-        val direction = if (currenY > initCoordinatesY) -1 else 1
-        job = CoroutineScope(Dispatchers.Default).launch {
-            delay(TIME_RESET_DELAY)
-            while (Math.abs(mY - initCoordinatesY) >= speed) {
-                mY += speed * direction
-                callBack?.invoke(mY)
-                delay(TIME_STEP_DELAY)
-            }
-            mY += Math.abs(mY - initCoordinatesY) * direction
-            callBack?.invoke(mY)
-        }
-    }
+//    private fun reset(currenY: Int) {
+//        val direction = if (currenY > initCoordinatesY) -1 else 1
+//        job = CoroutineScope(Dispatchers.Default).launch {
+//            delay(TIME_RESET_DELAY)
+//            while (Math.abs(mY - initCoordinatesY) >= speed) {
+//                mY += speed * direction
+//                callBack?.invoke(mY,mX)
+//                delay(TIME_STEP_DELAY)
+//            }
+//            mY += Math.abs(mY - initCoordinatesY) * direction
+//            callBack?.invoke(mY,mX)
+//        }
+//    }
 
-    fun setCallback(action: (Int) -> Unit) {
+    fun setCallback(action: (Int,Int) -> Unit) {
         callBack = action
     }
 
-    fun moveBalls(newY: Int) {
+    fun moveBalls(newX: Int,newY: Int) {
         mY = newY
+        mX = newX
     }
 
     companion object {
--- app/src/main/java/com/example/balls/customview/BouncyBalls.kt
+++ app/src/main/java/com/example/balls/customview/BouncyBalls.kt
@@ -1,23 +1,45 @@
 package com.example.balls.customview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
 import android.util.AttributeSet
 import android.util.Log
-import android.widget.GridLayout
-import android.widget.LinearLayout
+import android.view.MotionEvent
+import android.view.MotionEvent.*
+import android.view.View
 import com.example.balls.R
+import com.example.balls.customview.Ball.Companion.DEFAULT_SPEED
 import kotlinx.coroutines.*
-import java.util.*
+import java.lang.Math.abs
 
-class BouncyBalls : GridLayout {
+class BouncyBalls : View {
 
     private var listBalls = mutableListOf<Ball>()
+    private var ballsCount = DEFAULT_BALLS_COUNT
+    private var initCoordinatesX = 0
+    private var initCoordinatesY = 0
+    private var mWidth = 0
+    private var mHeight = 0
+    private var radius = 0F
+    private var state: Int = STATE_AUTO_ANI
+    private var jobMoveTo: Job? = null
+    private var jobFollow: Job? = null
+    private var jobBouncy: Job? = null
+    private var jobReset: Job? = null
+    val mPaintBall = Paint().apply {
+        setColor(Color.RED)
+        style = Paint.Style.FILL_AND_STROKE
+        strokeWidth = 10F
+        strokeCap = Paint.Cap.ROUND
+        isAntiAlias = true
+    }
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        var ballsCount = 0
         context.theme.obtainStyledAttributes(
             attrs,
             R.styleable.BouncyBalls,
@@ -25,43 +47,168 @@ class BouncyBalls : GridLayout {
         ).apply {
             try {
                 ballsCount = getInt(R.styleable.BouncyBalls_ballsCount, DEFAULT_BALLS_COUNT)
-                columnCount = ballsCount
-                rowCount = 1
             } finally {
                 recycle()
             }
         }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mWidth = MeasureSpec.getSize(widthMeasureSpec)
+        mHeight = MeasureSpec.getSize(heightMeasureSpec)
+        initCoordinatesY = mHeight / 2
+        initCoordinatesX = mWidth / 2
+        radius = mWidth / 5F / ballsCount
         initBalls(ballsCount)
+        setMeasuredDimension(mWidth, mHeight)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        listBalls.forEach { ball ->
+            drawBalls(canvas, ball)
+        }
+        startAutoBoucyBalls()
+        invalidate()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            ACTION_DOWN -> {
+                jobBouncy?.cancel()
+                jobMoveTo?.cancel()
+                jobFollow?.cancel()
+                jobReset?.cancel()
+                state = STATE_FOLLOW_ANI
+                listBalls.forEach { ball ->
+                    moveTo(ball.pos, ball.mX, ball.mY, event.x.toInt(), event.y.toInt())
+                }
+
+            }
+            ACTION_MOVE -> {
+                startFollowAni(event.x.toInt(), event.y.toInt())
+            }
+            ACTION_UP -> {
+                state = STATE_RESET_ANI
+                reset()
+            }
+        }
+        return true
     }
 
     private fun initBalls(ballsCount: Int) {
+        listBalls.clear()
         for (i in 0..ballsCount - 1) {
-            val ball = Ball(pos = i, numBalls = ballsCount,colorBall =getRandomColor(), context = context)
-            addView(ball)
+            val centerXY = caculCenterWithPossition(i)
+            val ball = Ball(
+                pos = i,
+                numBalls = ballsCount,
+                colorBall = getRandomColor(),
+                centerXY.first,
+                centerXY.second
+            )
             listBalls.add(ball)
         }
-        listBalls[0].setCallback { newY ->
+    }
+
+    private fun startFollowAni(newX: Int, newY: Int) {
+        listBalls[0].mX = newX
+        listBalls[0].mY = newY
+        jobFollow = CoroutineScope(Dispatchers.Default).launch {
             listBalls.forEach { ball ->
-                startChainBouncy(ball,newY)
+                delay(TIME_CHAIN_STEP_DELAY * 2)
+                ball.mX = newX
+                ball.mY = newY
+                invalidate()
             }
         }
     }
 
-    private fun startChainBouncy(ball: Ball,newY : Int){
-        CoroutineScope(Dispatchers.Default).launch {
-            delay(TIME_CHAIN_STEP_DELAY * ball.pos)
-            if (ball.pos > 0) {
-                ball.moveBalls(newY)
+    private fun drawBalls(canvas: Canvas?, ball: Ball) {
+        mPaintBall.setColor(ball.colorBall)
+        canvas?.drawCircle(
+            ball.mX.toFloat(), ball.mY.toFloat(), radius, mPaintBall
+        )
+    }
+
+    private fun startAutoBoucyBalls() {
+        //change direction of ball by y -> auto change ballY
+        if (state == STATE_AUTO_ANI) {
+            val firstBall = listBalls[0]
+            firstBall.mY += DEFAULT_SPEED * (firstBall.direction)
+            if (abs(firstBall.mY - initCoordinatesY / 2.toFloat()) < DEFAULT_SPEED)
+                firstBall.direction = 1
+            if (abs(firstBall.mY - 3 * initCoordinatesY / 2) < DEFAULT_SPEED)
+                firstBall.direction = -1
+            //follow chain
+            for (i in 1 until listBalls.size) {
+                val ball = listBalls[i]
+                val y = listBalls[i - 1].mY
+                jobBouncy = CoroutineScope(Dispatchers.Default).launch {
+                    delay(TIME_CHAIN_STEP_DELAY)
+                    ball.mY = y
+                }
             }
         }
     }
-    private fun getRandomColor(): Int{
+
+    private fun moveTo(pos: Int, oldX: Int, oldY: Int, newX: Int, newY: Int) {
+        val stepX = (newX - oldX) / 10
+        val stepY = (newY - oldY) / 10
+        jobMoveTo = CoroutineScope(Dispatchers.Default).launch {
+            for (i in 0..8) {
+                delay(MOVE_STEP_DELAY)
+                listBalls[pos].mX += stepX
+                listBalls[pos].mY += stepY
+            }
+            listBalls[pos].mX = newX
+            listBalls[pos].mY = newY
+        }
+    }
+
+    private fun reset() {
+        if (state == STATE_RESET_ANI) {
+            jobReset = CoroutineScope(Dispatchers.Default).launch {
+                jobFollow?.cancelAndJoin()
+                delay(RESET_TIME)
+                listBalls.forEach { ball ->
+                    moveTo(ball.pos, ball.mX, ball.mY, ball.centerX, ball.centerY)
+                }
+                jobMoveTo?.join()
+                state = STATE_AUTO_ANI
+            }
+        }
+    }
+
+    private fun getRandomColor(): Int {
         listColor.shuffle()
         return listColor[0]
     }
-    companion object{
-        const val TIME_CHAIN_STEP_DELAY = 100L
+
+    private fun caculCenterWithPossition(pos: Int): Pair<Int, Int> {
+        val cX = mWidth - (2 * pos + 1) * (mWidth / 2 / ballsCount)
+        val cY = mHeight / 2
+        return Pair(cX, cY)
+    }
+
+    companion object {
+        const val STATE_AUTO_ANI = 1
+        const val STATE_FOLLOW_ANI = 2
+        const val STATE_RESET_ANI = 3
+        const val TIME_CHAIN_STEP_DELAY = 50L
         const val DEFAULT_BALLS_COUNT = 5
-        val listColor = mutableListOf<Int>(Color.BLUE,Color.GREEN,Color.BLACK,Color.GRAY,Color.RED,Color.YELLOW)
+        const val DEFAULT_SPEED = 30
+        const val RESET_TIME = 1000L
+        const val MOVE_STEP_DELAY = 20L
+        val listColor = mutableListOf<Int>(
+            Color.BLUE,
+            Color.GREEN,
+            Color.BLACK,
+            Color.GRAY,
+            Color.RED,
+            Color.YELLOW
+        )
     }
 }
